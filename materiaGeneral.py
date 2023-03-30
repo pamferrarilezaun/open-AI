fc512b4ddf1346e54fb49c751e2f563a1cc9ad2c
Pamela Ferrari <pamela.lezaun@gmail.com>
2023-03-30 02:34:46 -0300

    first commit

---
+++ materiaGeneral.py
@@ -0,0 +1,65 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI, VectorDBQA
+from langchain.document_loaders import DirectoryLoader
+import magic
+import os
+import nltk
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+# nltk.download('averaged_perceptron_tagger')
+
+# Esto me sirvio para instalar las librerias.
+# pip install unstructured
+# Other dependencies to install https://langchain.readthedocs.io/en/latest/modules/document_loaders/examples/unstructured_file.html
+# pip install python-magic-bin
+# pip install chromadb
+
+# Se obtienen todos los archivos txt
+loader = DirectoryLoader('data/', glob='**/*.txt')
+print(loader)
+documents = loader.load()
+
+
+# Divide el texto de entrada en partes significativas para poder procesarlo.
+# Se divide en fragmentos con un tamanio de 1000 y el segundo parametro es paraa que no superpongan los fragmentos
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+# Se corte cada texto de entrada por documento
+texts = text_splitter.split_documents(documents)
+
+# print(texts)
+
+#Esto convierte de una cadena de caracteres a un espacio vectorial. Basicamente convierte palabras
+# a una lista valores numericos. 
+# Se utiliza chroma para crear un documento o un conjunto de vectores sobre el texto que se pasa en el primer
+# parametro y sobre el motor que esta embebido (pasado en el segundo parametro) 
+# Se realiza una consulta a la API de openAI
+embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
+docsearch = Chroma.from_documents(texts, embeddings)
+
+# En esta linea se inicializa nuestro modelo: se le pasa nuestro modelo de lenguaje (primer param.),
+# el segundo parametro 'Chain type of staff' significa que vamos a encontrar los fragmentos mas relevantes del texto
+# de todas las cadenas de texto que realizamos un split en la linea 28 
+# El tercer parametro es el vector de chroma
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
+
+query = "Hace un resumen juridico de los textos"
+
+respuesta = qa.run(query)
+print(respuesta)
+
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch, return_source_documents=True)
+query = "¿De que hablan los textos en su conjunto?"
+result = qa({"query": query})
+print(result['result'])
+
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch, return_source_documents=True)
+query = "Realiza un sumario juridico de cada una de los textos"
+result = qa({"query": query})
+print("sumario juridico:", result['result'])
+
+
+
---
+++ textosLargos.py
@@ -0,0 +1,97 @@
+# Documentacion de la libreria langchain: https://langchain.readthedocs.io/en/latest/getting_started/getting_started.html
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain import PromptTemplate
+
+# Pinecone es una plataforma de indexacion y recuperacion de vectores diseniada para manejar 
+# grandes cantidades de datos de alta dimensionalidad. 
+# Permite indexar vectores de datos y recuperar los vectores mas cercanos a una consulta dada en tiempo real, 
+# lo que la hace util en el procesamiento del lenguaje natural.
+import pinecone
+
+# Busca los txt de la carpeta a la que corresponde
+loader = DirectoryLoader('data/', glob='**/*.txt')
+# Lee los txt
+data = loader.load()
+# print(data[0])
+
+# Te cuenta con cuantos documentos estas trabajando y ademas te cuenta la cantidad de caracteres que tiene c/ doc.
+print (f'Hay {len(data)} documento(s) en data')
+print (f'Hay {len(data[0].page_content)} caracteres en el documento')
+
+# Divide los datos (en este caso el txt) en documentos mas pequeños
+# Se divide en fragmentos con un tamanio de 1000 y el segundo parametro es para que no superpongan los fragmentos
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# Se corta cada texto para que queden por separado
+texts = text_splitter.split_documents(data)
+print (f'Ahora hay {len(texts)} documentos') # Te cuenta cuantos documentos diferentes se formaron.
+# print("El primer texto es:", texts[0]) # Imprime el primer documento.
+
+# Se crean embeddings para cada uno de los documentos para la busqueda semantica
+OPENAI_API_KEY = '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'
+PINECONE_API_KEY = 'a72ad1a6-c685-459d-8adf-56cc1dc07c91'
+PINECONE_API_ENV = 'us-east-1-aws'
+# Si el server esta en estados unidos creo que deberia ir la siguiente linea: PINECONE_API_ENV = 'us-east1-gcp'
+
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+# iinicializacion de pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_API_ENV  # next to api key in console
+)
+index_name = "langchain2"
+
+# Utiliza el metodo Pinecone.from_texts() para crear embeddings de los textos en la lista de textos,
+# por cada objeto que tiene un atributo page_content realiza lo anterior. Estos embeddings creados 
+# se almacenan en un indice de Pinecone con el nombre especificado por index_name.
+docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
+print(docsearch)
+
+# La pregunta que va a buscar la IA en los textos.
+query = "¿A cuál clasificacion pertenece: derecho penal o derecho penal economico o derecho privado o derecho procesal o derecho procesal penal o derecho publico o derecho laboral? ¿Porque?"
+
+# Busca documentos similares semanticamente en el texto en base a la pregunta realizada. 
+docs = docsearch.similarity_search(query)
+
+print(docs)
+
+# A partir de aca se agrega langchain
+# Primer parametro: En este caso se utiliza el modelo curie
+# Segundo parametro: una temperatura de 0; Los valores mas altos como 0,8 haran que la salida sea mas aleatoria
+# mientras que los valores mas bajos como 0,2 la haran mas enfocada y determinista.
+#  Tercer parametro: son las credenciales de la API
+llm = OpenAI(model_name = "gpt-3.5-turbo", temperature=0.5, openai_api_key=OPENAI_API_KEY)
+
+# Definimos el prompt personalizado
+prompt_template = """Text: {context}
+Question: {question}"""
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+# Con este metodo se puede realizar dos tareas: utilizar el prompt y llamar al modelo de generacion de texto (openAi en este caso)
+chain = load_qa_chain(llm, chain_type="stuff", prompt=PROMPT)
+
+# Pregunta
+query = "¿A cuál clasificacion pertenece: derecho penal o derecho penal economico o derecho privado o derecho procesal o derecho procesal penal o derecho publico o derecho laboral? ¿Porque?"
+
+# Busca todos fragmentos dentro de los documentos mas similares a la pregunta que se hizo.
+docs = docsearch.similarity_search(query)
+
+# Aca se ingresan los documentos relevantes encontrados en la sentencia anterior y ademas la query
+respuesta_final = chain.run(input_documents=docs, question=query)
+print("respuesta final", respuesta_final)
+
+
+
+
+
+
+
+
